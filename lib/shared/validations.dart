39cbe26464f7a3d0e59d49eb0cd6dd5607039552
Kaique Cabral <kaique.11@hotmail.com>
2020-03-26 23:47:40 +0000

    Updating Files

--- lib/shared/validations.dart
+++
@@ -1,9 +0,0 @@
-bool validateEmail(String value) {
-  Pattern pattern =
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-  RegExp regex = new RegExp(pattern);
-  if (!regex.hasMatch(value))
-    return false;
-  else
-    return true;
-}
